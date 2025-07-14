bd5574a3754a00740f30734948e11e2665f0e543
Mukund <mukund199823@gmail.com>
2025-07-14 16:35:12 +0800

    replaced helpers in map manager with those from RM::utils::

--- include/rolling_map/map_manager.h
+++ include/rolling_map/map_manager.h
@@ -6,6 +6,7 @@
 #include "bonxai_map/occupancy_map.hpp"
 #include "rolling_map/chunk_manager.h"
 #include "rolling_map/map_params.h"
+#include "rolling_map/utils.h"
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -59,76 +60,6 @@ namespace RM
         void getOccupiedVoxels(PCLPointCloud& points);
 
     private:
-
-        /////////////////////////
-        //////Chunk Neibors//////
-        /////////////////////////
-
-        /**
-         * @brief Get the 6 face neibors of a chunk
-         * @param Bonxai::CoordT& c
-         * @return std::vector<Bonxai::CoordT>
-         */
-        std::array<Bonxai::CoordT,6> getFaceNeibors(const Bonxai::CoordT& coord);
-
-        /**
-         * @brief Get the 12 edge neibors of a chunk
-         * @param Bonxai::CoordT& c
-         * @return std::vector<Bonxai::CoordT>
-         */
-        std::array<Bonxai::CoordT,12> getEdgeNeibors(const Bonxai::CoordT& coord);
-
-        /**
-         * @brief Get the 8 corner neibors of a chunk
-         * @param Bonxai::CoordT& c
-         * @return std::vector<Bonxai::CoordT>
-         */
-        std::array<Bonxai::CoordT,8> getCornerNeibors(const Bonxai::CoordT& coord);
-
-        /////////////////////////
-        //////Conversions////////
-        /////////////////////////
-        /**
-         * @brief Converts a 3D point in map frame to a voxel coord. Exactly the same as Bonxai::posToCoord
-         * @param PCLPoint& point
-         * @return Bonxai::CoordT
-         */
-        Bonxai::CoordT mapPointToVoxelCoord(const PCLPoint& point);
-
-        /**
-         * @brief Converts a voxel coord to a 3D point in map frame. Exactly the same as Bonxai::coordToPos
-         * @param Bonxai::CoordT& coord
-         * @return PCLPoint
-         */
-        PCLPoint voxelCoordToMapPoint(const Bonxai::CoordT& coord);
-
-        /**
-         * @brief Convert a voxel coord to a 3D point in the map frame to the center of the voxel
-         * @param Bonxai::CoordT& coord
-         * @return PCLPoint
-         */
-        PCLPoint voxelCoordToMapCenterPoint(const Bonxai::CoordT& coord);
-
-        /**
-         * @brief COnverts a voxel coord to a chunk coordinate
-         * @param Bonxai::CoordT& coord
-         * @return CoordT
-         */
-        Bonxai::CoordT voxelCoordToChunkCoord(const Bonxai::CoordT& coord);
-
-        /**
-         * @brief Maps a chunk coordinate to the voxel coordinate, which is the back right down voxel in the map frame
-         * @param const Bonxai::CoordT& coord
-         * @return Bonxai::CoordT
-         */
-        Bonxai::CoordT chunkCoordToVoxelCoord(const Bonxai::CoordT& coord);
-
-        /**
-         * @brief Maps a 3D point in the map frame to a voxel coordinate that is relative to the voxel origin of the chunk
-         * @param const PCLPoint& point
-         * @return Bonxai::CoordT
-         */
-        Bonxai::CoordT mapFramePointToChunkFrameCoord(const PCLPoint& mP);
 
         //Chunk Manager
         std::unique_ptr<ChunkManager> chunk_manager_ {nullptr};
--- src/map_manager.cpp
+++ src/map_manager.cpp
@@ -23,10 +23,10 @@ namespace RM
     bool MapManager::updateMap(PCLPointCloud& points,PCLPoint& origin)
     {
         //The voxel coordinate where the origin is found in
-        auto voxel_coord_sensor = this->mapPointToVoxelCoord(origin);
+        auto voxel_coord_sensor = utils::mapPointToVoxelCoord(origin,mp_.resolution);
 
         //The chunk coordinate where the origin voxel is found in
-        auto chunk_coord_sensor = this->voxelCoordToChunkCoord(voxel_coord_sensor);
+        auto chunk_coord_sensor = utils::voxelCoordToChunkCoord(voxel_coord_sensor,cp_.chunk_dim);
 
         if (first_update_)
         {
@@ -41,7 +41,6 @@ namespace RM
             return able_to_update;
         }
     }
-
     
     void MapManager::getFreeVoxels(PCLPointCloud& points)
     {
@@ -52,124 +51,5 @@ namespace RM
     {
         chunk_manager_->getAllOccupiedVoxels(points);
     }
-
-    std::array<Bonxai::CoordT,6> MapManager::getFaceNeibors(const Bonxai::CoordT& coord)
-    {
-        //Follow the right-handle rule system X(front),Y(Left), Z(Up)
-        return {{
-            {coord.x + 1, coord.y, coord.z}, //front
-            {coord.x - 1, coord.y, coord.z}, //back
-            {coord.x, coord.y + 1, coord.z}, //left
-            {coord.x, coord.y - 1, coord.z}, //right
-            {coord.x, coord.y, coord.z + 1}, //top
-            {coord.x, coord.y, coord.z - 1}  //down
-        }};
-    }
-
-    std::array<Bonxai::CoordT,12> MapManager::getEdgeNeibors(const Bonxai::CoordT& coord)
-    {
-        //Follow the right-handle rule system X(front),Y(Left), Z(Up)
-        return {{
-            // xy-plane edges
-            {coord.x - 1, coord.y - 1, coord.z}, //back-right
-            {coord.x - 1, coord.y + 1, coord.z}, //back-left
-            {coord.x + 1, coord.y - 1, coord.z}, //front-right
-            {coord.x + 1, coord.y + 1, coord.z}, //front-left
-
-            // xz-plane edges
-            {coord.x - 1, coord.y, coord.z - 1}, //back-down
-            {coord.x - 1, coord.y, coord.z + 1}, //back-top
-            {coord.x + 1, coord.y, coord.z - 1}, //front-down
-            {coord.x + 1, coord.y, coord.z + 1}, //front-top
-
-            // yz-plane edges
-            {coord.x, coord.y - 1, coord.z - 1}, //right-down
-            {coord.x, coord.y - 1, coord.z + 1}, //right-top
-            {coord.x, coord.y + 1, coord.z - 1}, //left-down
-            {coord.x, coord.y + 1, coord.z + 1}  //left-top
-        }};
-    }
-
-    std::array<Bonxai::CoordT,8> MapManager::getCornerNeibors(const Bonxai::CoordT& coord) 
-    {
-        //Follow the right-handle rule system X(front),Y(Left), Z(Up)
-        return {{
-            {coord.x - 1, coord.y - 1, coord.z - 1}, //back  - right  -  down
-            {coord.x - 1, coord.y - 1, coord.z + 1}, //back  - right  -  top
-            {coord.x - 1, coord.y + 1, coord.z - 1}, //back  - left   -  down
-            {coord.x - 1, coord.y + 1, coord.z + 1}, //back  - left  -   top
-            {coord.x + 1, coord.y - 1, coord.z - 1}, //front - right  -  down
-            {coord.x + 1, coord.y - 1, coord.z + 1}, //front - right  -  top
-            {coord.x + 1, coord.y + 1, coord.z - 1}, //front - left   -  down
-            {coord.x + 1, coord.y + 1, coord.z + 1}  //front - left   -  top
-        }};
-    }
-
-    Bonxai::CoordT MapManager::mapPointToVoxelCoord(const PCLPoint& point)
-    {
-        return {
-            static_cast<int32_t>(std::floor(point.x * inv_resolution_)),
-            static_cast<int32_t>(std::floor(point.y * inv_resolution_)),
-            static_cast<int32_t>(std::floor(point.z * inv_resolution_))};
-    }
-
-    MapManager::PCLPoint MapManager::voxelCoordToMapPoint(const Bonxai::CoordT& coord)
-    {
-        return {
-            (static_cast<double>(coord.x)) * mp_.resolution,
-            (static_cast<double>(coord.y)) * mp_.resolution,
-            (static_cast<double>(coord.z)) * mp_.resolution};
-    }
-
-    MapManager::PCLPoint MapManager::voxelCoordToMapCenterPoint(const Bonxai::CoordT& coord)
-    {
-        double half = 0.5 * mp_.resolution;
-        return {
-            (static_cast<double>(coord.x)) * mp_.resolution + half,
-            (static_cast<double>(coord.y)) * mp_.resolution + half,
-            (static_cast<double>(coord.z)) * mp_.resolution + half};
-    }
-
-    Bonxai::CoordT MapManager::voxelCoordToChunkCoord(const Bonxai::CoordT& vc)
-    {
-        const int32_t chunk_size = cp_.chunk_dim;
-
-        return {
-        vc.x >= 0 ? vc.x / chunk_size : (vc.x - chunk_size + 1) / chunk_size,
-        vc.y >= 0 ? vc.y / chunk_size : (vc.y - chunk_size + 1) / chunk_size,
-        vc.z >= 0 ? vc.z / chunk_size : (vc.z - chunk_size + 1) / chunk_size
-        };
-    }
-
-    Bonxai::CoordT MapManager::chunkCoordToVoxelCoord(const Bonxai::CoordT& cc)
-    {
-        const int32_t chunk_size = cp_.chunk_dim;
-        return {
-            cc.x * chunk_size,
-            cc.y * chunk_size,
-            cc.z * chunk_size
-        };
-    }
-
-    Bonxai::CoordT MapManager::mapFramePointToChunkFrameCoord(const PCLPoint& mP)
-    {
-        using Coord = Bonxai::CoordT;
-
-        // Convert the Map Frame Point mP to Map Frame Voxel Coordinate mVC
-        Coord mVC = this->mapPointToVoxelCoord(mP);
-
-        // Find the Coordinate of the Chunk mCC
-        Coord mCC = this->voxelCoordToChunkCoord(mVC);
-
-        // Get the Origin Voxel in the Chunk as a Voxel Coordinate in map frame
-        Coord mOriginVC = this->chunkCoordToVoxelCoord(mCC);
-
-        // And Finally, make the mVC we calculated earlier relative to mOriginVC
-        Coord cVC = mVC - mOriginVC;
-
-        return cVC;
-
-    }
-
 
 }// namespace RM
